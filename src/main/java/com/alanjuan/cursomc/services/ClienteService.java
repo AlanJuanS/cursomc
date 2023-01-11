d2e9da773ae1df45714fbdfa7c50783b8baf78ac
alanjuan2302@gmail.com <ajuans@minsait.com>
2023-01-11 10:21:00 -0300

    endpoint/cliente/{id} disponivel

---
+++ src/main/java/com/alanjuan/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.alanjuan.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alanjuan.cursomc.domain.Cliente;
+import com.alanjuan.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		 return obj.orElseThrow(() -> new com.alanjuan.cursomc.services.exceptions.ObjectNotFoundException(
+				 "Objeto não encontrado! id: " + id + ", Tipo: " + Cliente.class.getName(), null));
+	
+	}
+			
+}
