9270e20c976118fb287c413eafd2aa3142ef9fbe
alanjuan2302@gmail.com <ajuans@minsait.com>
2023-01-17 14:20:59 -0300

    Listando todas as categoria com DTO

--- src/main/java/com/alanjuan/cursomc/services/CategoriaService.java
+++ src/main/java/com/alanjuan/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.alanjuan.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,9 @@ public class CategoriaService {
 		catch(DataIntegrityViolationException e ) {
 			throw new DataIntegrityException("não é possivel excluir uma categoria que possui produto");
 		}
+	}
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	
 }
