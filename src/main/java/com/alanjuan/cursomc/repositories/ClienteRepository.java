0c15dd36cb715e8a3a7fc080042fa7c5f30a2383
alanjuan2302@gmail.com <ajuans@minsait.com>
2023-02-15 14:42:56 -0300

    Validação customizada:não repetido na inserção de Cliente

--- src/main/java/com/alanjuan/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/alanjuan/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.alanjuan.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alanjuan.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 
 }
